221b2e7852f6474c56c0a1b94f19a22b66a7a9a2
ティデヴ <108774265+TyDevX@users.noreply.github.com>
2023-06-10 18:04:17 -0400

    some shit

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -26,7 +26,7 @@ class MainMenuState extends MusicBeatState
 	var menuItems:FlxTypedGroup<FlxSprite>;
 
 	#if !switch
-	var optionShit:Array<String> = ['story mode', 'freeplay', 'options', 'kickstarter'];
+	var optionShit:Array<String> = ['story mode', 'freeplay', 'kickstarter', 'options'];
 	#else
 	var optionShit:Array<String> = ['story mode', 'freeplay'];
 	#end
